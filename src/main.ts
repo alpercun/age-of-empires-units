2d75508c6dd91be230c849ab05fa0e2300696965
alpercun <alpercun@gmail.com>
2024-10-19 14:37:03 +0300

    Change theme

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,28 @@
 import './assets/main.scss';
+import '@mdi/font/css/materialdesignicons.css';
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import App from './App.vue';
+import router from './router';
+import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
-import 'vuetify/styles';
-import '@mdi/font/css/materialdesignicons.css';
-
-import App from './App.vue';
-import router from './router';
 
 const vuetify = createVuetify({
   components,
   directives,
+  theme: {
+    defaultTheme: 'dark',
+    themes: {
+      dark: {
+        colors: {
+          background: '#121212',
+        },
+      },
+    },
+  },
 });
 
 const app = createApp(App);
