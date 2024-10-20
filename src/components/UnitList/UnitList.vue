68e939814e513d1cb6046bb8a646feddce876d63
alpercun <alpercun@gmail.com>
2024-10-20 14:11:03 +0300

    Add visible columns feature (UnitList)

--- src/components/UnitList/UnitList.vue
+++ src/components/UnitList/UnitList.vue
@@ -1,19 +1,39 @@
 <template>
-  <v-data-table
-    class="unit-list"
-    :headers="headers"
-    :items="units"
-    @click:row="handleRowClick"
-  >
-    <template #[`item.cost`]="{ item }">
-      {{ formatCost(item.cost) }}
-    </template>
-  </v-data-table>
+  <div class="unit-list">
+    <div class="unit-list-column-visibility-controls-wrapper">
+      <v-select
+        class="unit-list-column-visibility-controls"
+        v-model="selectedColumns"
+        :items="headers"
+        item-title="title"
+        item-value="id"
+        item-class="custom-list-item"
+        multiple
+        label="Visible Columns"
+        prepend-icon="mdi-eye"
+        hide-details
+      />
+    </div>
+
+    <v-data-table-virtual
+      class="unit-list-table"
+      :headers="visibleHeaders"
+      :items="units"
+      :items-per-page="20"
+      height="400"
+      @click:row="handleRowClick"
+    >
+      <template #[`item.cost`]="{ item }">
+        {{ formatCost(item.cost) }}
+      </template>
+    </v-data-table-virtual>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { useRouter } from 'vue-router';
 import { useUnitsStore } from '@/stores/units';
+import { ref, computed } from 'vue';
 
 type Resource = 'Food' | 'Wood' | 'Gold';
 
@@ -28,9 +48,10 @@ interface Header {
   id: string;
   title: string;
   key: string;
+  value?: string;
 }
 
-defineProps<{
+const props = defineProps<{
   units: Unit[];
   headers: Header[];
 }>();
@@ -59,6 +80,14 @@ const handleRowClick = (event: Event, { item }: { item: Unit }) => {
   unitsStore.setSelectedUnit(item);
   router.push(`/units/${item.id}`);
 };
+
+const selectedColumns = ref(props.headers.map(header => header.id));
+
+const visibleHeaders = computed(() => {
+  return props.headers.filter(header =>
+    selectedColumns.value.includes(header.id),
+  );
+});
 </script>
 
 <style src="./UnitList.scss" lang="scss" scoped />
