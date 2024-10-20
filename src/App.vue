9bafe7489817ce6ee6828afdbe58eca914cccb61
alpercun <alpercun@gmail.com>
2024-10-20 15:28:02 +0300

    Update type error

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@
     <v-app-bar app>
       <v-toolbar-title>Age of Empires Units</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn to="/" text>Home</v-btn>
-      <v-btn to="/units" text>Units</v-btn>
+      <v-btn to="/" variant="text">Home</v-btn>
+      <v-btn to="/units" variant="text">Units</v-btn>
     </v-app-bar>
 
     <v-main>
