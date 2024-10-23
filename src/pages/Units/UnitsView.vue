768669b157b71e1d31acef784655cbe51ab13f3c
alpercun <alpercun@gmail.com>
2024-10-23 12:02:09 +0300

    Add min max constants (UnitsView)

--- src/pages/Units/UnitsView.vue
+++ src/pages/Units/UnitsView.vue
@@ -4,20 +4,20 @@
     <UnitAgeFilter v-model="unitsStore.selectedAges" :items="unitsStore.ages" />
     <ResourceCostFilter
       :type="unitsStore.costWood.type"
-      :min="0"
-      :max="200"
+      :min="MIN_COST"
+      :max="MAX_COST"
       @update="unitsStore.setCostWood"
     />
     <ResourceCostFilter
       :type="unitsStore.costFood.type"
-      :min="0"
-      :max="200"
+      :min="MIN_COST"
+      :max="MAX_COST"
       @update="unitsStore.setCostFood"
     />
     <ResourceCostFilter
       :type="unitsStore.costGold.type"
-      :min="0"
-      :max="200"
+      :min="MIN_COST"
+      :max="MAX_COST"
       @update="unitsStore.setCostGold"
     />
     <UnitList :units="unitsStore.units" :headers="headers" />
@@ -25,6 +25,9 @@
 </template>
 
 <script setup lang="ts">
+const MIN_COST = 0;
+const MAX_COST = 200;
+
 import { ref } from 'vue';
 import UnitList from '@/components/UnitList/UnitList.vue';
 import UnitAgeFilter from '@/components/UnitAgeFilter/UnitAgeFilter.vue';
