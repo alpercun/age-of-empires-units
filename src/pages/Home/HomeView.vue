245e107ba4bd6f61c9b13501890ed69842d08fa8
alpercun <alpercun@gmail.com>
2024-10-24 00:21:13 +0300

    Update img (HomeView)

--- src/pages/Home/HomeView.vue
+++ src/pages/Home/HomeView.vue
@@ -1,10 +1,9 @@
 <template>
   <v-container class="home-view">
-    <v-img
-      src="/src/assets/images/aoe-image.jpg"
+    <img
+      class="home-view-image"
+      src="@/assets/images/aoe-image.jpg"
       alt="Age of Empires"
-      width="100%"
-      height="400"
     />
   </v-container>
 </template>
